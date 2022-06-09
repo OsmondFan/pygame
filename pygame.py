228a3b64e814507967720a229bc936f858fffae5
OsmondFan <88888339+OsmondFan@users.noreply.github.com>
2022-06-09 18:39:12 +0800

    Initial commit

---
+++ pygame.py
@@ -0,0 +1,34 @@
+import os
+os.chdir('../')
+
+screen = None
+print('Hi')
+import site_packages.pygame
+
+class display:
+    def __init__(self):
+        self.screen = [[0,0],[0,0]]
+
+    def set_mode(self,size=(0,0),flags=0,depth=0,display=0,vsync=0):
+        self.screen = [[0,0],[size[0],size[1]]]
+
+    def flip(self):
+        pg.screen.flip()
+
+class universal:
+    def set_mode(self,size=(0,0),flags=0,depth=0,display=0,vsync=0):
+        global screen
+        screen = display.set_mode(size, flags, depth, display, vsync)
+
+
+class screen:
+    def __init__(self):
+        pass
+    def init(self):
+        pass
+
+    def update(self):
+        pass
+
+
+
