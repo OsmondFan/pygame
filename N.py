5028f2534e6a838298a038f1860b918b30e20d83
Ocillus <ocillus@outlook.com>
2022-06-28 18:48:46 +0800

    Screen.blit update

--- N.py
+++ N.py
@@ -1,10 +1,12 @@
 import importlib
 
+import pygame
 from pygame import*
 operating_system = None
 print('Hi')
 
 console = None
+scale_rate = (1,1)'''Meaning that the scale is 1x horizontal, 1x vertical'''
 
 class display:
     def init(self):
@@ -81,6 +83,50 @@ class screen:
     def update(self):
         pass
 
+    def blit(self,target=None,position=None):
+        '''
+        Again here in blitting, it couldn't just let pygame
+        blit away the stuff.
+
+        Blit has two parameters: target and position
+        :param target: the picture or sprite that you want to
+        move
+        :param position: the destination point of the target
+        after the move
+
+        Getting the target picture, we need to resize it
+        so that it fits inside the secondary window
+        We already know the scale rate from the display:
+        picture height=1 at first
+        we need to change the scale of it(or smoothscale)
+        pygame.transform.smoothscale(target,(target.get_width()*scale_rate[0],target.get_height()*scale_rate[1]))
+        Okay. So now we got the resized picture and we are going
+        to blit it onto the screen.
+        screen.blit(\'''the previous line things\''', ...)
+
+        moreover, the destination is actually just the same process
+        as the one of update() used in the display class
+        -->
+        if rectangle has parameters:
+        scales: if a window is (1000,1000) and the secondary window is (500,500)
+        the scale rate is: 2x, 2x
+        which means every component in the window should have a scale of 2x
+        component A is at (50,50), it then should be at (100,100) on main window
+        then blitting area should be [x*scale(2x)][y*scale(2x)]
+        if original rectangle update area
+        -->
+
+        Here we can use
+        screen.blit(..., (position[0]*scale_rate[0],position[1]*scale_rate[1]))
+
+
+
+        :return: None
+        '''
+        constant = pygame.transform.smoothscale(target,(target.get_width()*scale_rate[0],target.get_height()*scale_rate[1]))
+        screen.blit(constant, (position[0]*scale_rate[0],position[1]*scale_rate[1]))
+
+
 
 display = display()
 universal = universal()
@@ -94,3 +140,4 @@ display.flip()
 
 
 
+pygame.transform.smoothscale()
